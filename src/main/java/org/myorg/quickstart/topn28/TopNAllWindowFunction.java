50701eb7a5878d4e706f64dfbed1862bffa1b104
TZQ <zhengqiang.tan@zhangmen.com>
2020-10-23 11:33:08 +0800

    修复包引入错误

--- src/main/java/org/myorg/quickstart/topn28/TopNAllWindowFunction.java
+++ src/main/java/org/myorg/quickstart/topn28/TopNAllWindowFunction.java
@@ -2,17 +2,17 @@ package org.myorg.quickstart.topn28;
 
 
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.scala.function.ProcessAllWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
-import scala.collection.Iterable;
-import scala.collection.Iterator;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class TopNAllWindowFunction extends ProcessAllWindowFunction<OrderDetail,Tuple2<Double,OrderDetail>,TimeWindow>{
+
+public class TopNAllWindowFunction extends ProcessAllWindowFunction<OrderDetail, Tuple2<Double, OrderDetail>, TimeWindow> {
 
     private int size = 10;
 
@@ -20,11 +20,9 @@ public class TopNAllWindowFunction extends ProcessAllWindowFunction<OrderDetail,
         this.size = size;
     }
 
-
     @Override
     public void process(Context context, Iterable<OrderDetail> elements, Collector<Tuple2<Double, OrderDetail>> out) throws Exception {
-
-        TreeMap<Double,OrderDetail> treeMap = new TreeMap<Double,OrderDetail>(new Comparator<Double>() {
+        TreeMap<Double, OrderDetail> treeMap = new TreeMap<Double, OrderDetail>(new Comparator<Double>() {
             @Override
             public int compare(Double x, Double y) {
                 return (x < y) ? -1 : 1;
@@ -32,17 +30,18 @@ public class TopNAllWindowFunction extends ProcessAllWindowFunction<OrderDetail,
         });
 
         Iterator<OrderDetail> iterator = elements.iterator();
-        if(iterator.hasNext()){
-            treeMap.put(iterator.next().getPrice(),iterator.next());
-            if(treeMap.size() > 10){
+        if (iterator.hasNext()) {
+            treeMap.put(iterator.next().getPrice(), iterator.next());
+            if (treeMap.size() > 10) {
                 treeMap.pollLastEntry();
             }
         }
 
         for (Map.Entry<Double, OrderDetail> entry : treeMap.entrySet()) {
-            out.collect(Tuple2.of(entry.getKey(),entry.getValue()));
+            out.collect(Tuple2.of(entry.getKey(), entry.getValue()));
         }
 
-
     }
-}//
+
+}
+
